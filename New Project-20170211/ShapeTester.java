2f4a379572369aaf688407a628e443ee76316e03
nastassjamotro <nastassjamotro7@gmail.com>
2017-02-10 19:18:18 -0700

    Update ShapeTester.java

--- New Project-20170211/ShapeTester.java
+++ New Project-20170211/ShapeTester.java
@@ -11,7 +11,23 @@ public class ShapeTester{
         System.out.println("Enter height of box: ");
         boxH = scan.nextInt();
         Box box = new Box(boxL, boxW, boxH);
-        System.out.println("Surface Area of Box: " + box.getSurfaceArea);
-        System.out.println("Volume of Box: " + box.getVolume);
+        System.out.println("Surface Area of Box: " + box.getSurfaceArea());
+        System.out.println("Volume of Box: " + box.getVolume());
+        
+        int sphereR;
+        System.out.println("Enter radius of sphere: ");
+        sphereR = scan.nextInt();
+        Sphere sphere = new Sphere(sphereR);
+        System.out.println("Surface Area of Sphere: " + sphere.getSurfaceArea());
+        System.out.println("Volume of Sphere: " + sphere.getVolume());
+        
+        int pyramidH, pyramidB;
+        System.out.println("Enter height of pyramid: ");
+        pyramidH = scan.nextInt();
+        System.out.println("Enter base length of pyramid: ");
+        pyramidB = scan.nextInt();
+        Pyramid pyramid = new Pyramid(pyramidH, pyramidB);
+        System.out.println("Surface Area of Pyramid: " + pyramid.getSurfaceArea());
+        System.out.println("Volume of Sphere: " + pyramid.getVolume());
     }
 }
