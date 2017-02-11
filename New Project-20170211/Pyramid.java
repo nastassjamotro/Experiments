1476a75c07c8bfe40e29b5f87f461988ece73b7e
nastassjamotro <nastassjamotro7@gmail.com>
2017-02-10 19:01:29 -0700

    Create Pyramid.java

---
+++ New Project-20170211/Pyramid.java
@@ -0,0 +1,20 @@
+public class Pyramid {
+    private double height;
+    private double baseLength;
+    
+    public Pyramid(double h, double b) {
+        height = h;
+        baseLength = b;
+    }
+    
+    public double getSurfaceArea() {
+        double sideLength = Math.sqrt(height*height + baseLength*baseLength / 4);
+        double surfaceArea = 2 * sideLength * baseLength;
+        return surfaceArea;
+    }
+    
+    public double getVolume() {
+        double volume = height * baseLength * baseLength / 3;
+        return volume;
+    }
+}
