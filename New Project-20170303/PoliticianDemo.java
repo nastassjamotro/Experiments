fcc02f362910f3fc0b46eedb377a8802f1ea28a2
nastassjamotro <nastassjamotro7@gmail.com>
2017-03-03 16:54:42 -0700

    Update PoliticianDemo.java

--- New Project-20170303/PoliticianDemo.java
+++ New Project-20170303/PoliticianDemo.java
@@ -1,1 +1,12 @@
+package politician;
 
+public class PoliticianDemo {
+  public static void main(String[] args) {
+    // Politician generic = new Politician();
+    
+    Trump Donald = new Trump();
+    Clinton Hillary = new Clinton();
+    Donald.saySlogan();
+    Hillary.saySlogan();
+  }  
+}
