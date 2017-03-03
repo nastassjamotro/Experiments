7e4e19a24a14238bac3771b731216a9b0f90a6be
nastassjamotro <nastassjamotro7@gmail.com>
2017-03-03 16:37:54 -0700

    Update Shape.java

--- New Project-20170303/Shape.java
+++ New Project-20170303/Shape.java
@@ -1,1 +1,15 @@
+package shapes;
 
+import java.text.DecimalFormat;
+
+public abstract class Shape {
+  public abstract double area();
+  public abstract double perimeter();
+  public abstract class Shape {
+    public abstract double area();
+    public abstract double perimeter();
+    public String toString() {
+      DecimalFormat df = new DecimalFormat("#.##");
+      return "area = " + df.format(area()) + ", perimeter = " + df.format(perimeter());
+    }      
+  }    
