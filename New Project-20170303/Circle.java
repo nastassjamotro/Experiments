fbc78d6b0538143cd3c776fb6bf9efa37a26b4fe
nastassjamotro <nastassjamotro7@gmail.com>
2017-03-03 16:50:24 -0700

    Update Circle.java

--- New Project-20170303/Circle.java
+++ New Project-20170303/Circle.java
@@ -1,1 +1,17 @@
+package shapes;
 
+public class Circle extends Shape {
+  private double radius;
+  
+  public Circle(double _radius) {
+    radius = _radius;
+  }
+  
+  public double area() {
+    return Math.PI * radius * radius;
+  }
+  
+  public double perimeter() {
+    return 2 * Math.PI * radius;
+  }  
+}
