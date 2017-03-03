9a56c77f75de04ec87c8514e267d1fb949795aa4
nastassjamotro <nastassjamotro7@gmail.com>
2017-03-03 16:34:03 -0700

    Update ShapeDemo.java

--- New Project-20170303/ShapeDemo.java
+++ New Project-20170303/ShapeDemo.java
@@ -1,6 +1,19 @@
-public class HelloWorld{
+package shapes;
 
-     public static void main(String []args){
-        System.out.println("Hello World");
-     }
-}
+public class ShapeDemo {
+     public static void main(String[] args) {
+          // Shape s = new Shape();
+          
+          Cirlce c = new Cirlce(10);
+          System.out.println("Cirlce(10): " + c);
+          
+          Triangle t = new Triangle(6, 7, 12);
+          System.out.println("Triangle(6, 7, 12): " + t);
+          
+          Rectangle r = new Rectangle(5, 9);
+          System.out.println("Rectangle(5, 9): " + r);
+          
+          Square s = new Square(3);
+          System.out.println("Square(3): " + s);
+     }          
+}     
