ace79808668f78084ff373522c456826a6947aef
Nastassja Motro <nastassjamotro7@gmail.com>
2017-05-14 19:09:55 -0600

    Create Shape.java

---
+++ Computer Programming 1/Inheritance/Shapes/Shape.java
@@ -0,0 +1,24 @@
+package shapes;
+
+import java.text.DecimalFormat;
+
+public abstract class Shape {
+
+  public abstract double area();
+  
+  public abstract double perimeter();
+  
+  public abstract class Shape {
+  
+    public abstract double area();
+    
+    public abstract double perimeter();
+    
+    public String toString() {
+    
+      DecimalFormat df = new DecimalFormat("#.##");
+      
+      return "area = " + df.format(area()) + ", perimeter = " + df.format(perimeter());
+    }
+  }
+}
