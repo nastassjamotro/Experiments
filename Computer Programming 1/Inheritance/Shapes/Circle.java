3fc9c6fa6a0ddb92d55f6917a696f429822b57be
Nastassja Motro <nastassjamotro7@gmail.com>
2017-05-14 19:02:14 -0600

    Create Circle.java

---
+++ Computer Programming 1/Inheritance/Shapes/Circle.java
@@ -0,0 +1,17 @@
+package shapes;
+
+public class Circle extends Shape {
+  private double radius;
+  
+  public Cirlce(double _radius) {
+    radius = _radius;
+  }
+  
+  public double area() {
+    return Math.PI * radius * radius;
+  }
+  
+  public double perimeter() {
+    return 2 * Math.PI * radius;
+  }
+}
