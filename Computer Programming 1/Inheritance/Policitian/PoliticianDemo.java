9dacaaed5fadff9fc71e1f640d950f919b413527
Nastassja Motro <nastassjamotro7@gmail.com>
2017-05-14 18:53:16 -0600

    Create PoliticianDemo.java

---
+++ Computer Programming 1/Inheritance/Policitian/PoliticianDemo.java
@@ -0,0 +1,12 @@
+package politician;
+
+public class PoliticianDemo {
+  public static void main(String[] args) {
+    // Politician generic = new Politician();
+    
+    Trump Donald = new Trump();
+    Clinton Hillary = new clinton();
+    Donald.saySlogan();
+    Hillary.saySlogan();
+  }
+}
