2f45fd86ca96f8ce11ab51913fc828c74e1680d5
Nastassja Motro <nastassjamotro7@gmail.com>
2017-05-14 18:49:54 -0600

    Create Clinton.java

---
+++ Computer Programming 1/Inheritance/Policitian/Clinton.java
@@ -0,0 +1,15 @@
+package shapes;
+
+public class Clinton implements Politician {
+  public void saySlogan() {
+    System.out.println("Stronger Together");
+  }
+  
+  public double changeTaxes(double income, double initialRate) {
+    if(income > 300_000) {
+      return initialRate + 0.1;
+    } else {
+      return initialRate;
+    }
+  }
+}
